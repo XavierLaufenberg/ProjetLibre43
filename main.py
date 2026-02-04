719ca1c24539ba62ff7e184e36e115328b9c5ba7
XavierLaufenberg <xavier.laufenberg@magicmakers.fr>
2026-02-04 19:15:48 +0100

    séance 3 : connection BDD + prepa template

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 from flask import Flask, render_template
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+#charger les variables d'environnement
+load_dotenv()
 
 app = Flask("Pikapp")
 
+MONGO_URI = os.getenv('MONGO_URI')
+
+client = MongoClient(MONGO_URI)
+db = client.get_database("pikApp")
+
+
 @app.route('/')
 def index():
-    return render_template("index.html")
+    pokemon_data = list(db['pokemons'].find({}))
+    return render_template("index.html", pokemons = pokemon_data)
 
 app.run(host='0.0.0.0', port=81)
